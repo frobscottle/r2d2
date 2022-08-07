c0a2a6d8b079a1f7496565c1976f79ded24d65ce
Troy Dalldorf <tdalldorf@landolakes.com>
2022-08-07 15:55:15 -0500

    Test sounds

---
+++ temp/sound.py
@@ -0,0 +1,17 @@
+import os, time
+from pygame import mixer
+
+def playSound():
+        # Initialize pygame mixer
+    mixer.init()
+        # Load the sounds
+    sound = mixer.Sound('./sound/R2 beeping 2.mp3')
+        # play sounds
+    sound.play()
+         # wait for sound to finish playing
+    time.sleep(3)
+
+if __name__ == '__main__':
+    while(True):
+	playSound() 
+        time.sleep(10)
